8635105ea6780368bf42b8b3779d32e9daee49f0
Matthew <mattj20@gmail.com>
2022-06-23 07:34:30 -0700

    commit tree 82f8ae3bc2cc91fb1b0a270f970b48268e8fe422

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+    attr_accessor :weight, :height
+    attr_reader :name
+
+    @@all = []
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
